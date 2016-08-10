e2cabf8f312ebca025dfb3e2975a80685c48b9c9
trieudh <trieudh@icts.vn>
2016-08-10 16:03:23 +0700

    add: admin/courseRequests.addToPending() URI

--- back-end/controllers/admin/courseRequests.js
+++ back-end/controllers/admin/courseRequests.js
@@ -68,5 +68,68 @@ module.exports = {
                 });
             }
         });
+    },
+
+    /**
+     * @swagger
+     * path: /api/v1/admin/course-requests/add-to-pending
+     * operations:
+     *   -  httpMethod: PUT
+     *      summary: Add a public Course request to pending
+     *      notes: Return result
+     *      nickname: Add a public Course request to pending
+     *      consumes:
+     *        - text/html
+     *      parameters:
+     *        - name: x-access-token
+     *          description: Your token
+     *          paramType: header
+     *          required: true
+     *          dataType: string
+     *        - name: courseRequestId
+     *          description: Course request id
+     *          paramType: form
+     *          required: true
+     *          dataType: string
+     */
+    addToPending: function (req, res) {
+        models.CourseRequest.findById(req.body.courseRequestId, function (err, cr) {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    message: err
+                });
+            }
+            else if (!cr) {
+                res.json({
+                    success: false,
+                    message: 'Course request does not exist.'
+                });
+            }
+            else if (cr.status != 'Public') {
+                res.json({
+                    success: false,
+                    message: 'Course request is pending or denied.'
+                });
+            }
+            else {
+                cr.update({
+                    status: 'Pending'
+                }, function (err) {
+                    if (err) {
+                        res.status(500).json({
+                            success: false,
+                            message: err
+                        });
+                    }
+                    else {
+                        res.json({
+                            success: true,
+                            message: 'Course request is now pending'
+                        });
+                    }
+                });
+            }
+        });
     }
 };
--- back-end/routes/admin/courseRequests.js
+++ back-end/routes/admin/courseRequests.js
@@ -4,6 +4,10 @@ var AdminCourseRequestController = require('../../controllers/admin/courseReques
 var authentication = require('../../middleware/authentication');
 var adminPermission = require('../../middleware/adminPermission');
 
+/* Public one Course request */
 adminRouter.put('/public-one', authentication, adminPermission, AdminCourseRequestController.publicOne);
+
+/* Add one Course request to pending */
+adminRouter.put('/add-to-pending', authentication, adminPermission, AdminCourseRequestController.addToPending);
 
 module.exports = adminRouter;
