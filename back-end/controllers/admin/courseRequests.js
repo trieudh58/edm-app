0046264d359ad84f753453cd5fbb312a3f7b7f6e
trieudh <trieudh@icts.vn>
2016-08-10 15:51:21 +0700

    add: admin/courseRequests.publicOne() URI

--- back-end/controllers/admin/courseRequests.js
+++ back-end/controllers/admin/courseRequests.js
@@ -6,4 +6,67 @@ module.exports = {
      * resourcePath: /api/v1/admin/course-requests
      * description: Course request apis (for admin)
      */
+
+    /**
+     * @swagger
+     * path: /api/v1/admin/course-requests/public-one
+     * operations:
+     *   -  httpMethod: PUT
+     *      summary: Public one pending Course request
+     *      notes: Return result
+     *      nickname: Public one pending Course request
+     *      consumes:
+     *        - text/html
+     *      parameters:
+     *        - name: x-access-token
+     *          description: Your token
+     *          paramType: header
+     *          required: true
+     *          dataType: string
+     *        - name: courseRequestId
+     *          description: Course request id
+     *          paramType: form
+     *          required: true
+     *          dataType: string
+     */
+    publicOne: function (req, res) {
+        models.CourseRequest.findById(req.body.courseRequestId, function (err, cr) {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    message: err
+                });
+            }
+            else if (!cr) {
+                res.json({
+                    success: false,
+                    message: 'Course request does not exist.'
+                });
+            }
+            else if (cr.status != 'Pending') {
+                res.json({
+                    success: false,
+                    message: 'Course request is public or denied.'
+                });
+            }
+            else {
+                cr.update({
+                    status: 'Public'
+                }, function (err) {
+                    if (err) {
+                        res.status(500).json({
+                            success: false,
+                            message: err
+                        });
+                    }
+                    else {
+                        res.json({
+                            success: true,
+                            message: 'Course request is now public'
+                        });
+                    }
+                });
+            }
+        });
+    }
 };
--- back-end/routes/admin/courseRequests.js
+++ back-end/routes/admin/courseRequests.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var adminRouter = express.Router();
-var AdminCourseRequestController = require('../../controllers/courseRequests');
+var AdminCourseRequestController = require('../../controllers/admin/courseRequests');
 var authentication = require('../../middleware/authentication');
-var adminpermission = require('../../middleware/adminPermission');
+var adminPermission = require('../../middleware/adminPermission');
 
-
+adminRouter.put('/public-one', authentication, adminPermission, AdminCourseRequestController.publicOne);
 
 module.exports = adminRouter;
