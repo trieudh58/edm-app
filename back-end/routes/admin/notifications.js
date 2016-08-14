44c169e3364d4a2960d378d0d6a14ecbc8289dbf
trieudh <trieudh@icts.vn>
2016-08-14 09:12:00 +0700

    add validation.admin.notifications

--- back-end/routes/admin/notifications.js
+++ back-end/routes/admin/notifications.js
@@ -3,15 +3,16 @@ var adminRouter = express.Router();
 var AdminNotificationController = require('../../controllers/admin/notifications');
 var authentication = require('../../middleware/authentication');
 var adminPermission = require('../../middleware/adminPermission');
+var validation = require('../../validation/admin/notifications');
 
 /* Create a notification */
-adminRouter.post('/create', authentication, adminPermission, AdminNotificationController.createNew);
+adminRouter.post('/create', validation.createNew, authentication, adminPermission, AdminNotificationController.createNew);
 
 /* Send a created notification */
-adminRouter.put('/send-created', authentication, adminPermission, AdminNotificationController.sendCreated);
+adminRouter.put('/send-created', validation.sendCreated, authentication, adminPermission, AdminNotificationController.sendCreated);
 
 /* Create and send notification */
-adminRouter.post('/create-and-send', authentication, adminPermission, AdminNotificationController.createAndSend);
+adminRouter.post('/create-and-send', validation.createAndSend, authentication, adminPermission, AdminNotificationController.createAndSend);
 
 /* Get all notification created */
 adminRouter.get('/get-all', authentication, adminPermission, AdminNotificationController.getAll);
@@ -23,12 +24,12 @@ adminRouter.get('/get-all-sent', authentication, adminPermission, AdminNotificat
 adminRouter.get('/get-all-unsent', authentication, adminPermission, AdminNotificationController.getAllUnsent);
 
 /* Get a specific notification by id */
-adminRouter.get('/get-one-by-id', authentication, adminPermission, AdminNotificationController.getOneById);
+adminRouter.get('/get-one-by-id', validation.getOneById, authentication, adminPermission, AdminNotificationController.getOneById);
 
 /* Delete one notification by id */
-adminRouter.delete('/delete-one-by-id', authentication, adminPermission, AdminNotificationController.deleteOneById);
+adminRouter.delete('/delete-one-by-id', validation.deleteOneById, authentication, adminPermission, AdminNotificationController.deleteOneById);
 
 /* Delete one/many notifications by id */
-adminRouter.delete('/delete-by-ids', authentication, adminPermission, AdminNotificationController.deleteByIds);
+adminRouter.delete('/delete-by-ids', validation.deleteByIds, authentication, adminPermission, AdminNotificationController.deleteByIds);
 
 module.exports = adminRouter;
