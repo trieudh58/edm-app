739d49f923adfa9083c3215d08b24909f6fa2687
trieudh <trieudh@icts.vn>
2016-08-14 08:05:33 +0700

    put validation to subjects routes

--- back-end/routes/admin/subjects.js
+++ back-end/routes/admin/subjects.js
@@ -3,17 +3,18 @@ var adminRouter = express.Router();
 var AdminSubjectController = require('../../controllers/admin/subjects');
 var authentication = require('../../middleware/authentication');
 var adminPermission = require('../../middleware/adminPermission');
+var validation = require('../../validation/admin/subjects');
 
 /* Get AdminSubjectController' names and credits */
 adminRouter.get('/get-names-and-credits', authentication, adminPermission, AdminSubjectController.getNamesAndCredits);
 
 /* Get subject's information */
-adminRouter.get('/get-info', authentication,adminPermission, AdminSubjectController.getInfo);
+adminRouter.get('/get-info', validation.getInfo, authentication, adminPermission, AdminSubjectController.getInfo);
 
 /* Add subject name and credit */
-adminRouter.post('/create-subject', authentication, adminPermission, AdminSubjectController.createSubject);
+adminRouter.post('/create-subject', validation.createSubject, authentication, adminPermission, AdminSubjectController.createSubject);
 
 /* Delete a subject */
-adminRouter.delete('/delete-subject', authentication, adminPermission, AdminSubjectController.deleteSubject);
+adminRouter.delete('/delete-subject', validation.delete, authentication, adminPermission, AdminSubjectController.deleteSubject);
 
 module.exports = adminRouter;
