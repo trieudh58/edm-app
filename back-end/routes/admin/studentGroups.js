a5aae45571e9381565a1680793e89e5ec17110f3
trieudh <trieudh@icts.vn>
2016-08-14 08:03:32 +0700

    add validation.admin.studentGroups

--- back-end/routes/admin/studentGroups.js
+++ back-end/routes/admin/studentGroups.js
@@ -3,17 +3,18 @@ var adminRouter = express.Router();
 var StudentGroupController = require('../../controllers/admin/studentGroups');
 var authentication = require('../../middleware/authentication');
 var adminPermission = require('../../middleware/adminPermission');
+var validation = require('../../validation/admin/studentGroups');
 
 /* Get all groups */
 adminRouter.get('/get-all', authentication, adminPermission, StudentGroupController.getAll);
 
 /* Create new group */
-adminRouter.post('/create', authentication, adminPermission, StudentGroupController.create);
+adminRouter.post('/create', validation.create, authentication, adminPermission, StudentGroupController.create);
 
 /* Delete a group by id */
-adminRouter.delete('/delete-by-id', authentication, adminPermission, StudentGroupController.deleteById);
+adminRouter.delete('/delete-by-id', validation.deleteById, authentication, adminPermission, StudentGroupController.deleteById);
 
 /* Delete a group by name */
-adminRouter.delete('/delete-by-name', authentication, adminPermission, StudentGroupController.deleteByName);
+adminRouter.delete('/delete-by-name', validation.deleteByName, authentication, adminPermission, StudentGroupController.deleteByName);
 
 module.exports = adminRouter;
