f0607e331e4f9e0d606f7cdb7811695e16fd025b
trieudh <trieudh@icts.vn>
2016-08-22 21:48:49 +0700

    refactor: notifications.markOneAsUnread() URI

--- back-end/controllers/notifications.js
+++ back-end/controllers/notifications.js
@@ -419,8 +419,8 @@ module.exports = {
      *      consumes:
      *        - text/html
      *      parameters:
-     *        - name: x-access-token
-     *          description: Your token
+     *        - name: Authorization
+     *          description: Bearer [accessToken]
      *          paramType: header
      *          required: true
      *          dataType: string
@@ -446,13 +446,13 @@ module.exports = {
             }
         }).exec(function (err) {
             if (err) {
-                res.status(500).json({
+                return res.status(500).json({
                     success: false,
                     message: err
                 });
             }
             else {
-                res.json({
+                return res.json({
                     success: true,
                     message: 'Notification marked as unread.'
                 });
