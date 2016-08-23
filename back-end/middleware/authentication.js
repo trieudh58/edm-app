2cc380288e455ebb005a5dd9ce5289504d944e50
trieudh <trieudh@icts.vn>
2016-08-23 15:50:02 +0700

    refactor: authentication.js

--- back-end/middleware/authentication.js
+++ back-end/middleware/authentication.js
@@ -4,54 +4,64 @@ var config = require('../config/index');
 
 module.exports = function (req, res, next) {
     /* Get access token from request */
-    var accessToken = req.headers.authorization.split(' ')[1];
-    if (req.headers.authorization.split(' ')[0] !== 'Bearer') {
+    if (!req.headers.authorization || req.headers.authorization.split(' ')[0] !== 'Bearer') {
         return res.status(403).json({
             success: false,
             message: 'Invalid authorization.'
         });
     }
-    if (!accessToken) {
-        return res.status(403).json({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
     else {
-        jwt.verify(accessToken, config.jwt.secret, function (err, decoded) {
-            if (err || decoded.type !== 'accessToken') {
-                return res.status(400).json({
-                    success: false,
-                    message: 'Failed to authenticate. Invalid token.'
-                });
-            }
-            else {
-                models.User.findById(decoded.userId, function (err, user) {
-                    if (err) {
-                        return res.status(500).json({
+        var accessToken = req.headers.authorization.split(' ')[1];
+        if (!accessToken) {
+            return res.status(403).json({
+                success: false,
+                message: 'No token provided.'
+            });
+        }
+        else {
+            jwt.verify(accessToken, config.jwt.secret, function (err, decoded) {
+                if (err || decoded.type !== 'accessToken') {
+                    if (err.name === 'TokenExpiredError') {
+                        return res.status(403).json({
                             success: false,
-                            message: err
-                        });
-                    }
-                    else if (!user) {
-                        return res.status(400).json({
-                            success: false,
-                            message: 'Failed to authenticate. Invalid user.'
-                        });
-                    }
-                    else if (!user.isActive) {
-                        return res.status(400).json({
-                            success: false,
-                            message: 'Failed to authenticate. The account is deactivated.'
+                            message: err.name
                         });
                     }
                     else {
-                        req.user = user;
-                        req.accessToken = accessToken;
-                        next();
+                        return res.status(400).json({
+                            success: false,
+                            message: 'Failed to authenticate. Invalid token.'
+                        });
                     }
-                });
-            }
-        });
+                }
+                else {
+                    models.User.findById(decoded.userId, function (err, user) {
+                        if (err) {
+                            return res.status(500).json({
+                                success: false,
+                                message: err
+                            });
+                        }
+                        else if (!user) {
+                            return res.status(400).json({
+                                success: false,
+                                message: 'Failed to authenticate. Invalid user.'
+                            });
+                        }
+                        else if (!user.isActive) {
+                            return res.status(400).json({
+                                success: false,
+                                message: 'Failed to authenticate. The account is deactivated.'
+                            });
+                        }
+                        else {
+                            req.user = user;
+                            req.accessToken = accessToken;
+                            next();
+                        }
+                    });
+                }
+            });
+        }
     }
 };
