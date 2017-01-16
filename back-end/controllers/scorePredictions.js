b711b306e1f3422207b4a86d2e557a5f30cf38c5
trieudh <dangtrieu25@gmail.com>
2017-01-16 17:58:16 +0700

    add: score prediction for a list of subjects

--- back-end/controllers/scorePredictions.js
+++ back-end/controllers/scorePredictions.js
@@ -58,5 +58,76 @@ module.exports = {
                 });
             }
         });
+    },
+
+    /**
+     * @swagger
+     * path: /api/v1/score-predictions/predict-list-of-subjects
+     * operations:
+     *   -  httpMethod: GET
+     *      summary: Predict score for a list of subjects
+     *      notes: Return the score for a list of subjects
+     *      nickname: Predict scores for a subject list
+     *      consumes:
+     *        - text/html
+     *      parameters:
+     *        - name: Authorization
+     *          description: Bearer [accessToken]
+     *          paramType: header
+     *          required: true
+     *          dataType: string
+     *        - name: subjectCodeList
+     *          description: Subject code list (each separated by a comma)
+     *          paramType: query
+     *          required: true
+     *          dataType: string
+     */
+    /* Predict scores for a subject list */
+    predictListOfSubjects: function (req, res) {
+        var subjectCodeList = req.query.subjectCodeList.split(',');
+        if (subjectCodeList.length) {
+            models.Subject.find({
+                code: {
+                    $in: subjectCodeList
+                }
+            }, function (err, subjects) {
+                if (err)
+                    return handleInternalDBError(err, res);
+                if (!subjects.length)
+                    return handleUserErrorWithCustomMessage(res, 'Invalid subject code list');
+                var prediction = [];
+                for (var i = 0; i < subjects.length; i++) {
+                    prediction.push({
+                        subjectCode: subjects[i].code,
+                        subjectName: subjects[i].name,
+                        prediction: {
+                            score: (Math.random() * 9 + 1).toFixed(2),
+                            confidence: Math.random().toFixed(2)
+                        }
+                    });
+                }
+                return res.json({
+                    success: true,
+                    data: prediction
+                });
+            });
+        }
+        else {
+            return handleUserErrorWithCustomMessage(res, 'Invalid subject code list');
+        }
     }
 };
+
+function handleInternalDBError(err, res) {
+    return res.status(500).json({
+        success: false,
+        message: err
+    });
+}
+
+function handleUserErrorWithCustomMessage(res, customMessage) {
+    return res.status(400).json({
+        success: false,
+        message: customMessage
+    });
+}
--- back-end/validation/scorePredictions.js
+++ back-end/validation/scorePredictions.js
@@ -2,11 +2,27 @@ var Joi = require('joi');
 
 var validation = {};
 
-validation.predictOneSubject = function predictOneSubject(req, res, next) {
+validation.predictOneSubject = function predictOneSubject (req, res, next) {
     Joi.validate({
         subjectCode: req.query.subjectCode
     }, {
         subjectCode: Joi.string().required()
+    }, function (err) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err.details[0].message
+            });
+        }
+        next();
+    });
+};
+
+validation.predictListOfSubjects = function predictListOfSubjects (req, res, next) {
+    Joi.validate({
+        subjectCodeList: req.query.subjectCodeList
+    }, {
+        subjectCodeList: Joi.string().required()
     }, function (err) {
         if (err) {
             return res.json({
