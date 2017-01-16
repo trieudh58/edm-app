d7f822637a031a7725d9c36f3eb3d7a16212eae7
trieudh <dangtrieu25@gmail.com>
2017-01-16 11:03:56 +0700

    add: admins delete a feedback by id

--- back-end/controllers/admin/feedbacks.js
+++ back-end/controllers/admin/feedbacks.js
@@ -28,10 +28,48 @@ module.exports = {
     readAllFeedbacks: function (req, res) {
         models.Feedback.find({}, function (err, feedbacks) {
             if (err)
-                handleInternalDBError(err, res);
+                return handleInternalDBError(err, res);
             return res.json({
                 success: true,
                 data: feedbacks
+            });
+        })
+    },
+
+    /**
+     * @swagger
+     * path: /api/v1/admin/feedbacks/delete-one-by-id
+     * operations:
+     *   -  httpMethod: DELETE
+     *      summary: Admin deletes one feedback
+     *      notes: Return result
+     *      nickname: Delete one feedback by id
+     *      consumes:
+     *        - text/html
+     *      parameters:
+     *        - name: Authorization
+     *          description: Bearer [accessToken]
+     *          paramType: header
+     *          required: true
+     *          dataType: string
+     *        - name: feedbackId
+     *          description: Feedback id to be deleted
+     *          paramType: form
+     *          required: true
+     *          dataType: string
+     */
+    /* Delete one feedback by id */
+    deleteOneById: function (req, res) {
+        models.Feedback.remove({
+            _id: req.body.feedbackId
+        }, function (err, deletedRecord) {
+            if (err)
+                return handleInternalDBError(err, res);
+            if (!deletedRecord.result.n)
+                return handleUserErrorWithCustomMessage(res, 'Invalid id. No feedback deleted');
+            return res.json({
+                success: true,
+                message: 'Feedback deleted'
             });
         })
     }
@@ -41,5 +79,12 @@ function handleInternalDBError(err, res) {
     return res.status(500).json({
         success: false,
         message: err
+    });
+}
+
+function handleUserErrorWithCustomMessage(res, customMessage) {
+    return res.status(400).json({
+        success: false,
+        message: customMessage
     });
 }
--- back-end/routes/admin/feedbacks.js
+++ back-end/routes/admin/feedbacks.js
@@ -7,4 +7,7 @@ var adminPermission = require('../../middleware/adminPermission');
 /* Admins read all feedbacks */
 adminRouter.get('/read-all', authentication, adminPermission, AdminFeedbackController.readAllFeedbacks);
 
+/* Admins delete one feedback by id */
+adminRouter.delete('/delete-one-by-id', authentication, adminPermission, AdminFeedbackController.deleteOneById);
+
 module.exports = adminRouter;
