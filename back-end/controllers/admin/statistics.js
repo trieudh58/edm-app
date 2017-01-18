a536a7a7d76c1701288682beb995df04124d37a1
trieudh <dangtrieu25@gmail.com>
2017-01-18 09:20:22 +0700

    add: admins could get statistics of student quantity grouped by management class

--- back-end/controllers/admin/statistics.js
+++ back-end/controllers/admin/statistics.js
@@ -56,8 +56,56 @@ module.exports = {
                 }
             });
         });
-    }
+    },
     
+    /**
+     * @swagger
+     * path: /api/v1/admin/statistics/student-quantity-grouped-by-management-class
+     * operations:
+     *   -  httpMethod: GET
+     *      summary: Admin could get student quantity grouped by management class
+     *      notes: Return student quantity grouped by management class
+     *      nickname: Get student quantity grouped by management class
+     *      consumes:
+     *        - text/html
+     *      parameters:
+     *        - name: Authorization
+     *          description: Bearer [accessToken]
+     *          paramType: header
+     *          required: true
+     *          dataType: string
+     */
+    /* Get statistics of student quantity grouped by management class */
+    getStudentQuantityGroupedByManagementClass: function (req, res) {
+        models.User.aggregate([
+            {
+                $match: {
+                    isAdmin: false
+                }
+            },
+            {
+                $group: {
+                    _id: '$personalInfo.className',
+                    count: { $sum: 1 }
+                }
+            }
+        ], function (err, result) {
+            if (err)
+                return handleInternalDBError(err, res);
+            var managementClasses = [];
+            for (var i = 0; i < result.length; i++) {
+                managementClasses.push({
+                    className: result[i]._id,
+                    numberOfStudents: result[i].count
+                });
+            }
+            return res.json({
+                success: true,
+                data: managementClasses
+            });
+        });
+    }
+
 };
 
 function handleInternalDBError(err, res) {
--- back-end/routes/admin/statistics.js
+++ back-end/routes/admin/statistics.js
@@ -7,4 +7,7 @@ var adminPermission = require('../../middleware/adminPermission');
 /* Admins could get student detail */
 adminRouter.get('/student-accounts', authentication, adminPermission, AdminStatisticsController.getStudentAccounts);
 
+/* Admins could get student quantity grouped by management class */
+adminRouter.get('/student-quantity-grouped-by-management-class', authentication, adminPermission, AdminStatisticsController.getStudentQuantityGroupedByManagementClass);
+
 module.exports = adminRouter;
